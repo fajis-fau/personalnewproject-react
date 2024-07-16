9efcdfbf0660e0f5672081a8a8280241f80c0ee1
fajis-faju <fajisfaju793@gmail.com>
2024-07-16 16:25:49 +0300

    first commit

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -16,6 +16,7 @@ const Layout = () => {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/Gym-React-App' element={<Home />} />
           <Route path='/programs' element={<Programs />} />
           <Route path='/abouts' element={<About />} />
           <Route path='/plans' element={<Plans />} />
